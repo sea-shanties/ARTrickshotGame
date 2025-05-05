cb1d1545b15c528df71bcd14bcde99a95a5a12d8
sea-shanties <grace1.dmello@gmail.com>
2025-05-04 21:20:49 -0500

    Start button works; button click sounds added

--- AR_TrickshotGame/Assets/Scripts/MainMenuEvents.cs
+++ AR_TrickshotGame/Assets/Scripts/MainMenuEvents.cs
@@ -2,33 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class MainMenuEvents : MonoBehaviour
 {
     private UIDocument _document;
-
     private Button _button;
+    private List<Button> _menuButtons = new List<Button>();
+    private AudioSource _audioSource;
 
-    [SerializeField] private InputHandler _input;
+    [SerializeField] private string _startLevelName;
+
+    //[SerializeField] private InputHandler _input;
 
     private void Awake()
     {
+        _audioSource = GetComponent<AudioSource>();
         _document = GetComponent<UIDocument>();
 
         _button = _document.rootVisualElement.Q("StartButton") as Button;
-
         _button.RegisterCallback<ClickEvent>(OnPlayGameClick);
+
+        _menuButtons = _document.rootVisualElement.Query<Button>().ToList();
+        for (int i = 0; i < _menuButtons.Count; i++)
+        {
+            _menuButtons[i].RegisterCallback<ClickEvent>(OnAllButtonsClick);
+        }
     }
 
-    private void OnEnable()
-    {
-        _input.TouchStarted += OnTouchStarted;
-    }
+    //private void OnEnable()
+    //{
+    //    _input.TouchStarted += OnTouchStarted;
+    //}
 
     private void OnDisable()
     {
-        _input.TouchStarted -= OnTouchStarted;
-        // _button.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        //_input.TouchStarted -= OnTouchStarted;
+        _button.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+
+        for (int i = 0; i < _menuButtons.Count; i++)
+        {
+            _menuButtons[i].UnregisterCallback<ClickEvent>(OnAllButtonsClick);
+        }
     }
 
     private void OnTouchStarted(Vector2 position)
@@ -38,6 +53,12 @@ public class MainMenuEvents : MonoBehaviour
 
     private void OnPlayGameClick(ClickEvent evt)
     {
-        Debug.Log("Start button pressed");
+        // Debug.Log("Start button pressed");
+        SceneManager.LoadScene(_startLevelName);
+    }
+
+    private void OnAllButtonsClick(ClickEvent evt)
+    {
+        _audioSource.Play();
     }
 }
